8e737e31e06dada28199b80bb61afa57e4df5b80
daxuwudi <406631083@qq.com>
2019-03-05 11:30:55 +0800

    第二次提交

--- src/main/java/domea/repository/AuthorRepository.java
+++ src/main/java/domea/repository/AuthorRepository.java
@@ -2,7 +2,6 @@ package domea.repository;
 
 import domea.jpa.entities.Author;
 import org.springframework.data.repository.CrudRepository;
-
 public interface AuthorRepository  extends CrudRepository<Author ,Long> {
 
 }
